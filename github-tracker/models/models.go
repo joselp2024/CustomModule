75f0608f67da97f13b798d1b3be2370939ef9d63
joselp2024 <joselopez@popoyan.com>
2024-09-26 16:38:06 -0600

    corriendo pruebas

---
+++ github-tracker/models/models.go
@@ -0,0 +1,26 @@
+package models
+
+
+package models
+
+const REGION = "us-east-2"
+
+type GitHubWebhook struct {
+	Repository Repository `json:"repository"`
+	HeadCommit Commit     `json:"head_commit"`
+}
+
+type Repository struct {
+	FullName string `json:"full_name"`
+}
+
+type Commit struct {
+	ID      string     `json:"id"`
+	Message string     `json:"message"`
+	Author  CommitUser `json:"author"`
+}
+
+type CommitUser struct {
+	Email    string `json:"email"`
+	Username string `json:"username"`
+}
