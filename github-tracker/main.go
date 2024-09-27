f195195c316bd090d05e56da76c3ee2c50baab7a
joselp2024 <joselopez@popoyan.com>
2024-09-27 16:34:17 -0600

    add actions config

--- github-tracker/main.go
+++ github-tracker/main.go
@@ -1,10 +1,17 @@
 package main 
 
 import (
+	"context"
 	"fmt"
 	"io"
-	"net/http"  
+	"net/http"
+	"time"  
+
 	"github.com/gorilla/mux"
+
+	"github-tracker/github-tracker/models"
+	"github-tracker/github-tracker/repository"
+	"github-tracker/github-tracker/repository/entity"
 )
 
 
@@ -21,6 +28,23 @@ func postHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Println(string(body))
+}
+
+func insertGitHubWebhook(ctx context.Context, repo repository.Commit, webhook models.GitHubWebhook, body string, createdTime time.Time) error {
+	commit := entity.Commit{
+		RepoName:       webhook.Repository.FullName,
+		CommitID:       webhook.HeadCommit.ID,
+		CommitMessage:  webhook.HeadCommit.Message,
+		AuthorUsername: webhook.HeadCommit.Author.Username,
+		AuthorEmail:    webhook.HeadCommit.Author.Email,
+		Payload:        body,
+		CreatedAt:      createdTime,
+		UpdatedAt:      createdTime,
+	}
+
+	err := repo.Insert(ctx, &commit)
+
+	return err
 }
 
 func main() {
